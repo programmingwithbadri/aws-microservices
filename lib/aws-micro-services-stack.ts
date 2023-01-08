eeb018ac802f889d841a9f06bb1c0f74d4bf4c2e
Badri Narayanan <programmingwithbadri@outlook.com>
2023-01-08 16:04:20 +0530

    Create product table

--- lib/aws-micro-services-stack.ts
+++ lib/aws-micro-services-stack.ts
@@ -1,16 +1,20 @@
-import * as cdk from 'aws-cdk-lib';
+import {Stack,StackProps, RemovalPolicy} from 'aws-cdk-lib';
+import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class AwsMicroServicesStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class AwsMicroServicesStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsMicroServicesQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+ const productTable =new Table(this, 'product', {
+  partitionKey: {
+    name: 'id',
+    type: AttributeType.STRING
+  },
+  tableName: "product",
+  removalPolicy: RemovalPolicy.DESTROY,
+  billingMode: BillingMode.PAY_PER_REQUEST
+ })
   }
 }
