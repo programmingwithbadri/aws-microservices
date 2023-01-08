d6286615b49a2f4b2077431c5171f10d3807b528
Badri Narayanan <programmingwithbadri@outlook.com>
2023-01-08 16:29:49 +0530

    Create lambda function for product microservices

--- lib/aws-micro-services-stack.ts
+++ lib/aws-micro-services-stack.ts
@@ -1,20 +1,45 @@
 import {Stack,StackProps, RemovalPolicy} from 'aws-cdk-lib';
 import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
+import { Code, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction, NodejsFunctionProps } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
+import { join } from 'path';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AwsMicroServicesStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
- const productTable =new Table(this, 'product', {
-  partitionKey: {
-    name: 'id',
-    type: AttributeType.STRING
-  },
-  tableName: "product",
-  removalPolicy: RemovalPolicy.DESTROY,
-  billingMode: BillingMode.PAY_PER_REQUEST
- })
+    const productTable =new Table(this, 'product', {
+      partitionKey: {
+        name: 'id',
+        type: AttributeType.STRING
+      },
+      tableName: "product",
+      removalPolicy: RemovalPolicy.DESTROY,
+      billingMode: BillingMode.PAY_PER_REQUEST
+    });
+
+    const nodeJsFunctionProps: NodejsFunctionProps = {
+      bundling: {
+        externalModules:[
+          'aws-sdk'
+        ]
+      },
+      environment: {
+        PRIMARY_KEY: 'id',
+        DYNAMODB_TABLE_NAME: productTable.tableName,
+      },
+      runtime: Runtime.NODEJS_18_X
+    }
+
+    // Product micro services lambda function
+    const productLambdaFunction = new NodejsFunction(this, 'productLambdaFunction', {
+      entry: join(__dirname,'./../src/product/index.js'),
+      ...nodeJsFunctionProps
+    })
+
+    // grant read write access for the lambda
+    productTable.grantReadWriteData(productLambdaFunction)
   }
 }
