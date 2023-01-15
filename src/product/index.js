543aa1bf30dc3387b08900f413fcb2571cd912f0
Badri Narayanan <programmingwithbadri@outlook.com>
2023-01-15 22:18:08 +0530

    Create product with dynamo

--- src/product/index.js
+++ src/product/index.js
@@ -1,10 +1,35 @@
-const { GetItemCommand, ScanCommand } = require('@aws-sdk/client-dynamodb');
+const { GetItemCommand, ScanCommand, PutItemCommand } = require('@aws-sdk/client-dynamodb');
 const { marshall, unmarshall } = require('@aws-sdk/util-dynamodb');
 const { ddbClient } = require('./ddbClient');
+const { v4: uuidv4 } = require('uuid');
+
+const createProduct = async (event) => {
+    console.log(`createProduct function. event : "${event}"`);
+    try {
+        const productRequest = JSON.parse(event.body);
+        // set productid
+        const productId = uuidv4();
+        productRequest.id = productId;
+
+        const params = {
+            TableName: process.env.DYNAMODB_TABLE_NAME,
+            Item: marshall(productRequest || {})
+        };
+
+        const createResult = await ddbClient.send(new PutItemCommand(params));
+
+        console.log(createResult);
+        return createResult;
+
+    } catch (e) {
+        console.error(e);
+        throw e;
+    }
+}
 
 const getProduct = async (productId) => {
     try {
-        const parmas = {
+        const params = {
             TableName: process.env.DYNAMODB_TABLE_NAME,
             key: marshall({ id: productId })
         }
@@ -17,7 +42,7 @@ const getProduct = async (productId) => {
 
 const getAllProducts = async () => {
     try {
-        const parmas = {
+        const params = {
             TableName: process.env.DYNAMODB_TABLE_NAME,
         }
         const { Items } = await ddbClient.send(new ScanCommand(params))
@@ -35,6 +60,11 @@ exports.handler = async (event) => {
             } else {
                 body = await getAllProducts()
             }
+        case "POST":
+            body = await createProduct(event); // POST /product
+            break;
+        default:
+            throw new Error(`Unsupported route: "${event.httpMethod}"`);
     }
     return {
         statusCode: 200,
