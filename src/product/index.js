db71e9110c08f523706348059e9814da5db4de4e
Badri Narayanan <programmingwithbadri@outlook.com>
2023-01-15 22:26:42 +0530

    update and delete products

--- src/product/index.js
+++ src/product/index.js
@@ -1,8 +1,58 @@
-const { GetItemCommand, ScanCommand, PutItemCommand } = require('@aws-sdk/client-dynamodb');
+const { GetItemCommand, ScanCommand, PutItemCommand, UpdateItemCommand, DeleteItemCommand } = require('@aws-sdk/client-dynamodb');
 const { marshall, unmarshall } = require('@aws-sdk/util-dynamodb');
 const { ddbClient } = require('./ddbClient');
 const { v4: uuidv4 } = require('uuid');
 
+const deleteProduct = async (productId) => {
+    console.log(`deleteProduct function. productId : "${productId}"`);
+
+    try {
+        const params = {
+            TableName: process.env.DYNAMODB_TABLE_NAME,
+            Key: marshall({ id: productId }),
+        };
+
+        const deleteResult = await ddbClient.send(new DeleteItemCommand(params));
+
+        console.log(deleteResult);
+        return deleteResult;
+    } catch (e) {
+        console.error(e);
+        throw e;
+    }
+}
+
+const updateProduct = async (event) => {
+    console.log(`updateProduct function. event : "${event}"`);
+    try {
+        const requestBody = JSON.parse(event.body);
+        const objKeys = Object.keys(requestBody);
+        console.log(`updateProduct function. requestBody : "${requestBody}", objKeys: "${objKeys}"`);
+
+        const params = {
+            TableName: process.env.DYNAMODB_TABLE_NAME,
+            Key: marshall({ id: event.pathParameters.id }),
+            UpdateExpression: `SET ${objKeys.map((_, index) => `#key${index} = :value${index}`).join(", ")}`,
+            ExpressionAttributeNames: objKeys.reduce((acc, key, index) => ({
+                ...acc,
+                [`#key${index}`]: key,
+            }), {}),
+            ExpressionAttributeValues: marshall(objKeys.reduce((acc, key, index) => ({
+                ...acc,
+                [`:value${index}`]: requestBody[key],
+            }), {})),
+        };
+
+        const updateResult = await ddbClient.send(new UpdateItemCommand(params));
+
+        console.log(updateResult);
+        return updateResult;
+    } catch (e) {
+        console.error(e);
+        throw e;
+    }
+
+}
 const createProduct = async (event) => {
     console.log(`createProduct function. event : "${event}"`);
     try {
@@ -62,6 +112,12 @@ exports.handler = async (event) => {
             }
         case "POST":
             body = await createProduct(event); // POST /product
+            break;
+        case "DELETE":
+            body = await deleteProduct(event.pathParameters.id); // DELETE /product/{id}
+            break;
+        case "PUT":
+            body = await updateProduct(event); // PUT /product/{id}
             break;
         default:
             throw new Error(`Unsupported route: "${event.httpMethod}"`);
