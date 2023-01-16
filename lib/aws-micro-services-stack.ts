27172bb10ae930f1bd18fe84bd24ba21ffb2e516
Badri Narayanan <programmingwithbadri@outlook.com>
2023-01-16 12:15:47 +0530

    Implement apiGateway infra solution

--- lib/aws-micro-services-stack.ts
+++ lib/aws-micro-services-stack.ts
@@ -1,6 +1,6 @@
-import {Stack,StackProps, RemovalPolicy} from 'aws-cdk-lib';
-import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
+import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { SwnApiGateway } from './apigateway';
 import { SwnDatabase } from './database';
 import { SwnMicroservices } from './microservice';
 
@@ -8,27 +8,14 @@ export class AwsMicroServicesStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const database = new SwnDatabase(this, 'Database'); 
-   
+    const database = new SwnDatabase(this, 'Database');    
+
     const microservices = new SwnMicroservices(this, 'Microservices', {
       productTable: database.productTable
     });
 
-    const productApiGateWay = new LambdaRestApi(this, 'productApi', {
-      restApiName: 'Product Service',
-      handler: microservices.productMicroservice,
-      proxy: false // proxy: true will send all the requests to the lambda
-    })
-
-    const productApi = productApiGateWay.root.addResource('product');
-    productApi.addMethod('GET'); // GET '/product
-    productApi.addMethod('POST'); // POST '/product
-    
-    const singleProductApi = productApi.addResource('{id}');
-    singleProductApi.addMethod('GET'); // GET '/product/{id}
-    singleProductApi.addMethod('PUT'); // PUT '/product/{id}
-    singleProductApi.addMethod('DELETE'); // DELETE '/product/{id}
-
-    
+    const apigateway = new SwnApiGateway(this, 'ApiGateway', {
+      productMicroservice: microservices.productMicroservice,
+    });    
   }
 }
